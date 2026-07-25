36fa051dfd9699e968cd919dbd78404de8813391
Tushar Kumar <167692314+curioustushaar@users.noreply.github.com>
2026-07-25 10:48:59 +0530

    Time: 12 ms (23.38%), Space: 46.9 MB (65.26%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,21 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int ans = nums[i];
-            int count = 0;
+        // Frequency count
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
 
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == ans) {
-                    count++;
-                }
-            }
-
-            if (count == 1) {
-                return ans;
+        // Find frequency 1
+        for (int num : nums) {
+            if (map.get(num) == 1) {
+                return num;
             }
         }
 
-        return -1; // Agar koi single number na mile
+        return -1;
     }
 }
