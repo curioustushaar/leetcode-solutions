87942dac55c94771ac2db6011d72443c02a19c38
Tushar Kumar <167692314+curioustushaar@users.noreply.github.com>
2026-08-11 19:21:56 +0530

    Time: 1 ms (58.74%), Space: 48.7 MB (22.2%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int n = height.length;
+
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+
+        prefixMax[0] = height[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = Math.max(prefixMax[i - 1], height[i]);
+        }
+
+        suffixMax[n - 1] = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMax[i] = Math.max(suffixMax[i + 1], height[i]);
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += Math.min(prefixMax[i], suffixMax[i]) - height[i];
+        }
+
+        return total;
+    }
+}
